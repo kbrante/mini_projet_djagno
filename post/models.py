a19b044fa8e2c42ded172025c87c665396b39ce7
salwa <salwaoudad@gmail.com>
2017-05-11 14:19:22 +0200

    add FK category

--- post/models.py
+++ post/models.py
@@ -6,17 +6,6 @@ from django.db import models
 from autoslug import AutoSlugField
 
 # Create your models here.
-class Post(models.Model):
-    title = models.CharField(verbose_name='Mon super titre', max_length=100)
-    slug = AutoSlugField(populate_from='title')
-    body = models.TextField(verbose_name='Contenu')
-    creation_date = models.DateTimeField(auto_now_add=True)
-
-    def __unicode__(self):
-        return self.title
-
-    class Meta:
-        verbose_name = 'Article'
 
 class Category(models.Model):
     label = models.CharField(verbose_name='nom de categorie', max_length=50)
@@ -26,3 +15,18 @@ class Category(models.Model):
 
     class Meta:
         verbose_name = 'Categorie'
+
+
+
+class Post(models.Model):
+    title = models.CharField(verbose_name='Mon super titre', max_length=100)
+    slug = AutoSlugField(populate_from='title')
+    body = models.TextField(verbose_name='Contenu')
+    creation_date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category)
+
+    def __unicode__(self):
+        return self.title
+
+        class Meta:
+            verbose_name = 'Article'
