0e58d7e599b192ed10139651108c026684114211
karina BRANTE <karina.brante@gmail.com>
2017-05-11 15:22:32 +0200

    customization admin

--- post/admin.py
+++ post/admin.py
@@ -5,7 +5,23 @@ from django.contrib import admin
 
 from models import Post, Category
 
+class PostAdmin(admin.ModelAdmin):
+    model = Post
+    list_display = ('id','title', 'creation_date', 'category')
+    list_filter = ('category',)
+    search_fields = ('title', 'body')
+    list_editable = ('title', 'category',)
+
+    fieldsets = (
+        ('None', {
+            'fields': (('title', 'category'), 'body'),
+        }),
+        ('contenu',{
+            'fields': ('body',),
+            'classes': ('collapse',),
+        }),
+    )
 # Register your models here.
 
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
